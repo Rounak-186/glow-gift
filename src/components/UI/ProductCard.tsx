0c62bc096a94f2df82a2a07a5ed07aa47977b1d9
Rounak <rounakmohata0186@gmail.com>
2026-01-05 19:43:53 +0530

    feat: Wishlist page added

--- src/components/UI/ProductCard.tsx
+++ src/components/UI/ProductCard.tsx
@@ -1,10 +1,9 @@
-import { Heart, Star } from 'lucide-react'
+import { Heart, HeartOff, Share, Share2, ShoppingCart, Star } from 'lucide-react'
 import { Button } from './Button';
 
-export default function ProductCard() {
+export default function ProductCard({ isWishlistProduct = false }: { isWishlistProduct?: boolean }) {
 
     const percentage = Math.min(Math.max(4.7 / 5 * 100, 0), 100);
-
 
     return (
         <div className='rounded-lg shadow-md flex flex-col items-center overflow-hidden'>
@@ -17,10 +16,19 @@ export default function ProductCard() {
                 <Button className='absolute top-6 left-5 !px-2 !py-0 rounded-full bg-[var(--secondary)] opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300'>
                     <span className='text-gray-800'>Anime</span>
                 </Button>
-                {/* save to wishlist */}
-                <Button className='absolute top-5 right-5 !p-2 !rounded-full opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300'>
-                    <span className=''><Heart /></span>
-                </Button>
+                {/* save to wishlist or share */}
+                {
+                    isWishlistProduct ? (
+                        <Button className='absolute top-5 right-5 !p-2 !rounded-full opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300'>
+                            <span className=''><HeartOff /></span>
+                        </Button>
+                    ) : (
+
+                        <Button className='absolute top-5 right-5 !p-2 !rounded-full opacity-0 scale-90 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300'>
+                            <span className=''><Heart /></span>
+                        </Button>
+                    )
+                }
             </div>
             {/* rating and reviews */}
             <div className='p-4 '>
@@ -52,16 +60,27 @@ export default function ProductCard() {
                     <div className='text-gray-500 font-semibold '>186 reviews</div>
                 </div>
                 {/* Product Title and description*/}
-                <div className='flex flex-col items-start mb-3'>
+                <div className='flex flex-col items-start mb-2'>
                     <strong className='text-2xl'>Naruto Glass painting</strong>
                     <p className='line-clamp-2 text-gray-400 text-sm mt-1 text-left'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis similique fugit quae labore nobis perferendis.</p>
                 </div>
                 {/* price and add to cart */}
                 <div className='flex items-center justify-between'>
                     <span className='text-[var(--primary)] text-xl font-bold'>{"\u20B9"}399</span>
-                    <Button variant='primary' className='font-bold'>
-                        Add to Cart
-                    </Button>
+                    <div className='flex items-center gap-3'>
+                        {
+                            isWishlistProduct && (
+                                <Button className=' text-gray-700! !p-2 opacity-100 scale-100 ' variant='nav'>
+                                    <span className=''><Share2 /></span>
+                                </Button>
+                            )
+                        }
+                        <Button variant='primary' className='font-bold flex items-center justify-center gap-3'>
+                            <span className="flex justify-center items-center"><ShoppingCart size={20} strokeWidth={3}/></span>
+                            <span>Add to Cart</span>
+                        </Button>
+
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/pages/Wishlist.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ProductCard from "../components/UI/ProductCard";
+import { Button } from "../components/UI/Button";
+import { ShoppingBag, ShoppingCart } from "lucide-react";
+
+export const WishlistPage = () => {
+    return (
+        <div className="h-full">
+            {/* headerPart */}
+            <div className="w-full bg-linear-to-br from-(--primary) to-(--secondary) p-10 text-white flex justify-between items-center">
+                <div className="text-left">
+                    <h1 className="font-semibold text-4xl ">My Wishlist</h1>
+                    <p className="">7 Items saved for later</p>
+                </div>
+                <div className="flex items-center justify-center gap-4">
+                    <Button variant="outline" className="rounded-full! py-1! ">
+                        <span>Continue Shopping</span>
+                        <span className="flex justify-center items-center"><ShoppingBag size={20} strokeWidth={2} /></span>
+                    </Button>
+                    <Button variant="primary" className="rounded-full! py-1! flex items-center gap-5 justify-between">
+                        <span>View Cart</span>
+                        <span className="flex justify-center items-center"><ShoppingCart size={20} strokeWidth={2} /></span>
+                    </Button>
+                </div>
+            </div>
+            {/* products */}
+            <div className="mx-auto w-full grid grid-cols-3 h-full gap-8 max-w-6xl my-5">
+                {Array(5).fill(null).map((_, i) => {
+                    return (
+                        <ProductCard isWishlistProduct={true} key={i} />
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
